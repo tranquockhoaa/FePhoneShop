fc73fd0d1b1842a668ac8de97ecf6620e01c37be
duc-max <tranminhducpchy@gmail.com>
2025-08-20 14:59:52 +0700

    pull code

--- src/pages/auth/ForgotPassword.jsx
+++ src/pages/auth/ForgotPassword.jsx
@@ -1,16 +1,16 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Button, notification, Form, Input } from 'antd';
-import { forgotPasswordApi, resetPasswordApi } from '../../api/profile.api';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Button, notification, Form, Input } from "antd";
+import { forgotPasswordApi, resetPasswordApi } from "../../api/profile.api";
 
 function ForgotPassword() {
   const navigate = useNavigate();
-  const [email, setEmail] = useState('');
+  const [email, setEmail] = useState("");
 
   const [api, contextHolder] = notification.useNotification();
 
   const handleCancel = () => {
-    navigate('/login');
+    navigate("/login");
   };
 
   const onFinishSendCode = async (values) => {
@@ -18,32 +18,32 @@ function ForgotPassword() {
       await forgotPasswordApi(values);
       setEmail(values.email);
       api.success({
-        message: 'Email',
-        description: 'Vui lòng kiểm tra email để nhận mã xác thực',
+        message: "Email",
+        description: "Vui lòng kiểm tra email để nhận mã xác thực",
       });
     } catch (error) {
       api.error({
-        message: 'Error',
-        description: 'Error',
+        message: "Error",
+        description: error.data.error,
       });
     }
   };
 
   const onFinishChangePassword = async (values) => {
-    console.log('Success:', values);
+    console.log("Success:", values);
 
     try {
       await resetPasswordApi({ ...values, email });
 
       api.success({
-        message: 'Thành công',
-        description: 'Đổi mật khẩu thành công',
+        message: "Thành công",
+        description: "Đổi mật khẩu thành công",
       });
-      navigate('/login');
+      navigate("/login");
     } catch (error) {
       api.error({
-        message: 'Error',
-        description: 'Error',
+        message: "Error",
+        description: error.data.error,
       });
     }
   };
@@ -51,49 +51,61 @@ function ForgotPassword() {
   return (
     <div className="forgotPassword-container">
       {contextHolder}
-      <h2>Quên Mật Khẩu</h2>
+      <h2 style={{ color: "#3b82f6" }}>Quên Mật Khẩu</h2>
       <div className="separator"></div>
       <p className="description">Nhập email để lấy lại mật khẩu</p>
       {email ? (
-        <Form
-          name="basic"
-          onFinish={onFinishChangePassword}
-        >
+        <Form name="basic" onFinish={onFinishChangePassword}>
           <Form.Item
             name="code"
-            rules={[{ required: true, message: 'Code is required' }]}
+            rules={[{ required: true, message: "Code is required" }]}
           >
             <Input placeholder="Nhập mã code" />
           </Form.Item>
           <Form.Item
             name="newPassword"
             rules={[
-              { required: true, message: 'Please input your new password!' },
+              { required: true, message: "Please input your new password!" },
             ]}
           >
             <Input.Password />
+          </Form.Item>
+          
+          <Form.Item
+            name="confirmPassword"
+            dependencies={['newPassword']}
+            rules={[
+              { 
+                required: true, 
+                message: 'Please confirm your password!' 
+              },
+              ({ getFieldValue }) => ({
+                validator(_, value) {
+                  if (!value || getFieldValue('newPassword') === value) {
+                    return Promise.resolve();
+                  }
+                  return Promise.reject('The two passwords that you entered do not match!');
+                },
+              }),
+            ]}
+          >
+            <Input.Password placeholder="Confirm Password" />
           </Form.Item>
 
           <div className="wrap-button">
             <Button onClick={handleCancel}>Hủy</Button>
 
-            <Button
-              type="primary"
-              htmlType="submit"
-            >
+            <Button type="primary" htmlType="submit">
               Tiếp tục
             </Button>
           </div>
         </Form>
       ) : (
-        <Form
-          name="basic"
-          onFinish={onFinishSendCode}
-        >
+        <Form name="basic" onFinish={onFinishSendCode}>
           <Form.Item
             name="email"
             rules={[
-              { required: true, type: 'email', message: 'Email is required' },
+              { required: true, type: "email", message: "Email is required" },
             ]}
           >
             <Input placeholder="Nhập địa chỉ email" />
@@ -102,10 +114,7 @@ function ForgotPassword() {
           <div className="wrap-button">
             <Button onClick={handleCancel}>Hủy</Button>
 
-            <Button
-              type="primary"
-              htmlType="submit"
-            >
+            <Button type="primary" htmlType="submit">
               Tiếp tục
             </Button>
           </div>
--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,29 +1,31 @@
-import React from 'react';
-import '../../index.css';
-import { Link } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { getUserProfileRequest } from '../../store/profile/profile.action';
+import React from "react";
+import "../../index.css";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { getUserProfileRequest } from "../../store/profile/profile.action";
 
-import { loginApi } from '../../api/profile.api';
-import { createCart } from '../../api/cart-user';
+import { loginApi } from "../../api/profile.api";
+import { createCart } from "../../api/cart-user";
 
 function Login() {
   let navigate = useNavigate();
   const dispatch = useDispatch();
   const { profile } = useSelector((state) => state.profile);
   const [account, setAccount] = useState({
-    email: '',
-    password: '',
-    status: '',
+    email: "",
+    password: "",
+    status: "",
   });
+
+  const [error, setError] = useState("");
 
   useEffect(() => {
     // Lấy role từ localStorage sau khi đăng nhập
 
-    if (profile?.role === 'admin') navigate('/admin');
-    else if (profile?.role === 'user') navigate('/');
+    if (profile?.role === "admin") navigate("/admin");
+    else if (profile?.role === "user") navigate("/");
     // Nếu có role khác, có thể bổ sung điều hướng tại đây
   }, [profile]);
 
@@ -35,26 +37,29 @@ function Login() {
     event.preventDefault();
 
     try {
+      if (!account.email || !account.password) {
+        setError("Vui lòng nhập đủ thông tin");
+        return;
+      }
+
+      setError("");
       const dataLogin = await loginApi(account);
-      setAccount({ ...account, status: 'true' });
-      window.localStorage.setItem('token', dataLogin.token);
+      setAccount({ ...account, status: "true" });
+      window.localStorage.setItem("token", dataLogin.token);
       dispatch(getUserProfileRequest());
-      createCart()
+      createCart();
       // Handle successful login here if needed
-      console.log('Login successful:', dataLogin);
+      console.log("Login successful:", dataLogin);
     } catch (error) {
-      // todo
-      console.error('Login error:', error);
+      setError(error.data.message);
+      console.error("Login error:", error);
     }
   };
 
   return (
     <div className="login-container">
       <h2 className="login-title">Đăng nhập</h2>
-      <form
-        className="login-form"
-        onSubmit={handleOnSubmit}
-      >
+      <form className="login-form" onSubmit={handleOnSubmit}>
         <div className="input-wrapper">
           <input
             type="email"
@@ -62,7 +67,6 @@ function Login() {
             name="email"
             placeholder="Nhập địa chỉ email"
             className="input-field"
-            required
           ></input>
         </div>
 
@@ -73,33 +77,32 @@ function Login() {
             placeholder="Nhập mật khẩu"
             name="password"
             className="input-field"
-            required
           />
         </div>
 
         <div className="forgot-pass-link">
+          {error ? (
+            <span style={{ color: "red" }}>{error}</span>
+          ) : (
+            <span></span>
+          )}
+
           <span>
-            <Link
-              to="/login/forgotPassword"
-              className="forgot-pass-link"
-            >
+            <Link to="/login/forgotPassword" className="forgot-pass-link">
               Quên mật khẩu
             </Link>
           </span>
         </div>
 
         <div>
-          <button
-            type="submit"
-            className="login-button"
-          >
+          <button type="submit" className="login-button">
             Đăng nhập
           </button>
         </div>
       </form>
 
       <div className="sign-in">
-        Bạn chưa có tài khoản?{' '}
+        Bạn chưa có tài khoản?{" "}
         <span>
           <Link to="/signIn">Đăng kí ngay</Link>
         </span>
